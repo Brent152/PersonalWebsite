222e648179790aaa15ea7ec5d110384ee3386339
Brent152 <60564806+Brent152@users.noreply.github.com>
2022-12-29 00:49:23 -0700

    Fix target = _blank error

--- personal-website/pages/components/GitHubLogo.tsx
+++ personal-website/pages/components/GitHubLogo.tsx
@@ -3,7 +3,7 @@
 export default function GitHubLogo(props: any) {
 
     return (
-        <a href={props.link} target={'_blank'}>
+        <a href={props.link} target={'_blank'} rel="noreferrer">
             <img src='../../GitHubLogo.png' style={{ width: 30, height: 30 }} />
         </a>
     )
